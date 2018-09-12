9b63cfefe4c444122670f20d48613350d230a8fa
simplysameer333 <samorsameer@gmail.com>
2018-09-12 17:58:35 +0100

    Update config.py

--- POC/config.py
+++ POC/config.py
@@ -2,3 +2,13 @@
 base_path = '../data/cnn/'
 path = base_path + 'stories/sample/'
 stories_pickle_filename = "news.pickle"
+
+# to avoid words that are used less that threshold value
+threshold = 5
+# Dimension size as per pre-trained data
+embedding_dim = 300
+
+max_text_length = 84
+max_summary_length = 13
+min_length = 2
+unk_text_limit = 1
