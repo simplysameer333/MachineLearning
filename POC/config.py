71cf5012267a8e51b464dd27f15394b428fc2ab1
sameer <samorsameer@gmail.com>
2018-09-13 20:53:09 +0100

    Adding data pre-processing

--- POC/config.py
+++ POC/config.py
@@ -1,14 +1,27 @@
-#base_path = 'G:\\AI\\data\\cnn\\'
-base_path = '../data/cnn/'
+base_path = 'G:\\AI\\data\\cnn\\'
+#base_path = '../data/cnn/'
 path = base_path + 'stories/sample/'
+#path = base_path + 'stories/'
 stories_pickle_filename = "news.pickle"
+word_embedding_matrix_filename = "word_embedding_matrix.pickle"
+
+# model_path ='G:\Python\MLLearning\MachineLearning\data\wiki-news-300d-1M.vec'
+model_path= 'C:\Temp\python_files\MLLearning\data\wiki-news-300d-1M.vec'
 
 # to avoid words that are used less that threshold value
-threshold = 5
+threshold = 2
+
 # Dimension size as per pre-trained data
 embedding_dim = 300
-
-max_text_length = 84
-max_summary_length = 13
+max_text_length = 1000
+max_summary_length = 20
 min_length = 2
-unk_text_limit = 1
+unk_text_limit = 200
+
+# Set the Hyperparameters
+epochs = 100
+batch_size = 64
+rnn_size = 256
+num_layers = 2
+learning_rate = 0.005
+keep_probability = 0.75
